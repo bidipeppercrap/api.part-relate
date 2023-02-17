eafb406317c9afa389b41016cf31aa00cb225cff
Fransisco Wijaya <bidipeppercrap@outlook.com>
2023-02-17 20:35:26 +0700

    enable cors

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 import { initializeTables } from "./db.ts";
 import router from "./routes/index.ts";
@@ -7,5 +8,7 @@ const app = new Application();
 
 await initializeTables();
 
+app.use(oakCors());
 app.use(router.routes());
-await app.listen({ port: 8000 });
+
+await app.listen({ port: 3000 });
